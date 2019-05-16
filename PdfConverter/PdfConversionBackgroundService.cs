b24670e323c46ed6c1b38596cc3879235e5408ca
Taylor Southwick <tasou@microsoft.com>
2019-05-16 15:40:07 -0700

    Add project files.

---
+++ PdfConverter/PdfConversionBackgroundService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PdfConverter
+{
+    public interface IConversionQueue
+    {
+        Task<Stream> ConvertAsync(Stream input, CancellationToken token);
+    }
+
+    internal class ConversionQueue : IConversionQueue
+    {
+        private readonly ConcurrentQueue<(Stream stream, TaskCompletionSource<Stream> tcs)> _queue = new ConcurrentQueue<(Stream stream, TaskCompletionSource<Stream>)>();
+        private readonly SemaphoreSlim _signal = new SemaphoreSlim(0);
+
+        public Task<Stream> ConvertAsync(Stream input, CancellationToken token)
+        {
+            var tcs = new TaskCompletionSource<Stream>();
+            _queue.Enqueue((input, tcs));
+            _signal.Release();
+            return tcs.Task;
+        }
+
+        public async Task<(Stream stream, TaskCompletionSource<Stream> tcs)> DequeueAsync(CancellationToken token)
+        {
+            await _signal.WaitAsync(token);
+
+            _queue.TryDequeue(out var result);
+            return result;
+        }
+    }
+
+    internal class PdfConversionBackgroundService : BackgroundService
+    {
+        private readonly ConversionQueue _queue;
+        private readonly PdfConverter _converter;
+
+        public PdfConversionBackgroundService(ConversionQueue queue, PdfConverter converter)
+        {
+            _queue = queue;
+            _converter = converter;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var (stream, tcs) = await _queue.DequeueAsync(stoppingToken);
+
+
+            }
+        }
+    }
+}
---
+++ PdfConverter/PdfConverter.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Office.Interop.Word;
+using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace PdfConverter
+{
+    internal class PdfConverter : IDisposable
+    {
+        private readonly Application _application;
+        private readonly DisablePdfWarning _registry;
+
+        public PdfConverter()
+        {
+            _application = new Application
+            {
+
+            };
+
+            _registry = new DisablePdfWarning(_application.Version);
+
+#if DEBUG
+            _application.Visible = true;
+#endif
+        }
+
+        public void Convert(string path, string newPath = null)
+        {
+            var document = _application.Documents.Open(path, ConfirmConversions: false, Visible: true);
+
+            if (string.IsNullOrEmpty(newPath))
+            {
+                newPath = Path.ChangeExtension(path, ".docx");
+            }
+
+            document.SaveAs2(newPath);
+            document.Close();
+        }
+
+        ~PdfConverter()
+        {
+            _application.Quit();
+            _registry.Dispose();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            _application.Quit();
+            _registry.Dispose();
+        }
+    }
+    internal sealed class DisablePdfWarning : IDisposable
+    {
+        private const string PdfWarningNameKeyName = "DisableConvertPdfWarning";
+
+        private readonly RegistryKey _key;
+        private readonly object _value;
+
+        public DisablePdfWarning(string version)
+        {
+            _key = Registry.CurrentUser.OpenSubKey(Path.Combine("SOFTWARE", "Microsoft", "Office", version, "Word", "options"), true);
+            _value = _key?.GetValue(PdfWarningNameKeyName);
+            _key.SetValue(PdfWarningNameKeyName, 1);
+        }
+
+        public void Dispose()
+        {
+            if (_value is null)
+            {
+                _key.DeleteValue(PdfWarningNameKeyName);
+            }
+            else
+            {
+                _key.SetValue(PdfWarningNameKeyName, _value);
+            }
+
+            _key.Dispose();
+        }
+    }
+}
